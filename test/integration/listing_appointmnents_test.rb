4396e0b7cbe86b68483ec1f034983c9da7e49a6e
Matthew Roche <MJRoche@usf.edu>
2015-11-22 15:06:08 -0500

    validation for listing

---
+++ test/integration/listing_appointmnents_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class ListingAppointmentsTest < ActionDispatch::IntegrationTest
+  # setup code here
+  setup { host! 'example.com'}
+
+  test 'returns a list of appointments' do
+    get '/appointments'
+    assert_equal 200, response.status
+    refute_empty response.body
+  end
+
+end
