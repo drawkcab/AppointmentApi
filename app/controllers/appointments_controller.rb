54250af5153505c8fa21732c29f25faabed21fd5
Matthew Roche <MJRoche@usf.edu>
2015-11-23 00:47:28 -0500

    added delete function.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -38,6 +38,9 @@ class AppointmentsController < ApplicationController
   end
 
   def destroy
+    appointment = Appointment.find(params[:id])
+    appointment.destroy
+    head 204
   end
 
   private
